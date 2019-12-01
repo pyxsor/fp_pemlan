522be2e56dc2cd03c80fb0a8a40a1bbbcd252cee
pyxsor <hanzdeveloper@gmail.com>
2019-12-01 23:22:33 +0700

    Version 1.1

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 	Final Project
+	Nama Anggota Kelompok 8:
+	- Ayunda Noviala Dwijayanti (18081010025)
+	- Handie Pramana Putra (18081010023)
+	
+	Algoritma:
+	Sorting 	: Quick Sort
+	Searching 	: Binary Search
+	
+	Aplikasi Jasa Pengiriman (no.5)
+	
+	 */
+
+int main() {
+	
+	
+	return 0;
+}
